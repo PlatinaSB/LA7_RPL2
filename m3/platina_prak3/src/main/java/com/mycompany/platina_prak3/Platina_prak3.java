ab40ab00efaf448123e6b287b743b19f00e24c3c
PlatinaSB <59110147+PlatinaSB@users.noreply.github.com>
2025-12-04 20:12:36 +0700

    Projek Minggu 3

---
+++ m3/platina_prak3/src/main/java/com/mycompany/platina_prak3/Platina_prak3.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.platina_prak3;
+
+/**
+ *
+ * @author Platina Setia Budi
+ */
+public class Platina_prak3 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
